eaf8d2e146e1e4a919116c47718414dd7fb5c3b3
Aryan <aryan10kumar11@gmail.com>
2025-05-05 12:21:26 +0530

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar";
-import Carousel from "./Carousel";
-import About from "./About";
-import Stats from "./Stats";
-import ServiceSlider from "./ServiceSlider";
-import Mobile from "./Mobile";
-import Calculator from "./Calculator";
-import Blog from "./Blog";
-import Gallery from "./Gallery";
-import Youtube from "./Youtube";
-import ImpactStories from "./ImpactStories";
 import Footer from "./Footer";
 import AboutPage from "./Aboutpage"; // Import the AboutPage component
 import Landing from "./Landing";
